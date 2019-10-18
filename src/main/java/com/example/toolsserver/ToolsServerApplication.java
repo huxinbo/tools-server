db7d9da2c44fa15f5566bbf4832d811a1ccb2655
huxinbo <huxinbo56@163.com>
2019-10-18 17:29:06 +0800

    init commit

---
+++ src/main/java/com/example/toolsserver/ToolsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.toolsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toolsserver/ToolsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toolsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
