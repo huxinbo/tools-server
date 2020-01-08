18ebf6d046ebc44604bd6afcb4d0d4ab6ab0b608
huxinbo <huxinbo56@163.com>
2020-01-08 11:17:18 +0800

    修改Mysql配置

--- src/main/java/com/example/toolsserver/category/Category.java
+++ src/main/java/com/example/toolsserver/category/Category.java
@@ -4,13 +4,18 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Version;
 import java.time.Instant;
 
-@Data
-@Document(indexName = "tools", type = "category", shards = 1, replicas = 0)
+//@Document(indexName = "tools", type = "category", shards = 1, replicas = 0)
+@Entity
+@Table(name = "category")
 public class Category {
     @Id
     private Long id = 0L;
+    @Version
     private Integer version = 0;
     private Instant createAt = Instant.EPOCH;
     private Instant updateAt = Instant.EPOCH;
