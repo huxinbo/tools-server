c3885af7661a2ab161fd32bdb13f8e471acffb2e
huxinbo <huxinbo56@163.com>
2020-01-15 13:12:02 +0800

    调整项目

--- src/main/java/com/example/toolsserver/ToolsServerApplication.java
+++ src/main/java/com/example/toolsserver/ToolsServerApplication.java
@@ -2,10 +2,10 @@ package com.example.toolsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ToolsServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ToolsServerApplication.class, args);
     }
